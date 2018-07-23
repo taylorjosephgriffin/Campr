bf018634817616fd5f0c94f3db21b8c79d34ef7e
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-23 15:55:07 -0700

    Wrapped navbar brand in a span element to cut down on styling rules. #1

--- client/nav.js
+++ client/nav.js
@@ -5,8 +5,10 @@ export default function Campgrounds(props) {
   return (
     <nav className='navbar navbar-light bg-primary p-0'>
       <a className='navbar-brand ml-4 text-light' href='campground-list'>
-        <i className='fas fa-fire logo-icon'></i>
-        <span className='logo-name pl-2'>Campr</span>
+        <span className='nav-logos'>
+          <i className='fas fa-fire logo-icon'></i>
+          <span className='logo-name pl-2'>Campr</span>
+        </span>
       </a>
     </nav>
   )
