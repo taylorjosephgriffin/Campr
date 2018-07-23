fc22188c031ec6abd82addb0a92738a04684f7ab
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-23 15:26:24 -0700

    Implented a bootswatch theme and adjusted components to use it. #1

--- client/nav.js
+++ client/nav.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Campgrounds(props) {
 
   return (
-    <nav className='navbar navbar-light bg-light p-0'>
-      <a className='navbar-brand ml-3' href='campground-list'>
+    <nav className='navbar navbar-light bg-primary p-0'>
+      <a className='navbar-brand ml-4 text-light' href='campground-list'>
         <i className='fas fa-fire logo-icon'></i>
         <span className='logo-name pl-2'>Campr</span>
       </a>
