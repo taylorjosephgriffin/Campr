6908880e7fe0609febc8f197a622a949ea92fb8a
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-23 15:31:51 -0700

    Refactored card head styling to be more responsive.

--- client/campgrounds.js
+++ client/campgrounds.js
@@ -6,14 +6,14 @@ export default function Campgrounds(props) {
     <div className='container-fluid'>
       <div className='row'>
       { props.campgrounds.map((camps, index) =>
-        <div key={index} className='col-xl-4 mt-4'>
+        <div key={index} className='col-xl-4 col-lg-6 mt-4'>
           <div className='card h-100'>
             <div
               className='card-img-top campground-list-image h-100'
               style={{backgroundImage:`url(${camps.facilityPhotos[0]})`,
               backgroundPosition: 'center',
               backgroundSize: 'cover'}}>
-                <h5 className='card-title campground-name text-light text-center display-4'>{camps.facilityName}</h5>
+                <h5 className='card-title campground-name text-light text-center'>{camps.facilityName}</h5>
               </div>
             <div className='card-body'>
               <p className='card-text h5'>{`$${camps.price}.00 / night`}</p>
