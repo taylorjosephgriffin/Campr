c26bb52e8e2135b24b9cb0440562bc8ee3f44f34
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-24 13:30:45 -0700

    Enhanced campground-router.js to include a get method for finding a campground by id. #2

--- server/routes/campground-router.js
+++ server/routes/campground-router.js
@@ -10,5 +10,12 @@ module.exports = function campgroundRouter(collection) {
       .then(campgrounds => res.json(campgrounds))
       .catch(err => console.error(err))
   })
+
+  router.get('/:id', (req, res) => {
+    collection
+      .findOne({ id: req.params.id })
+      .then(campground => res.json(campground))
+      .catch(err => console.error(err))
+  })
   return router
 }
