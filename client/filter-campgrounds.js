231b3291faf67cf7ebe09b368311fb48cba6fb73
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-27 10:11:11 -0700

    Converted Filter back into a stateless component. #4

--- client/filter-campgrounds.js
+++ client/filter-campgrounds.js
@@ -2,43 +2,49 @@ import React from 'react'
 import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap'
 
 export default function Filter(props) {
-
   return (
     <div className='container-fluid'>
       <div className='row justify-content-end'>
-        <div className='col-xl-1'>
+        <div className='col-lg-6'>
           <button id='Popover1' className='nav-link filter-link bg-transparent border-0 text-primary h4 ml-2 mt-2 float-right' onClick={props.toggle}><i className='fas fa-filter'></i></button>
+            {props.filter.map((filter, index) =>
+              filter.price
+                ? <span key={ index } className='badge badge-primary float-right mr-2 mt-4 text-center'>{`$${filter.price}`}</span>
+                : <span key={ index } className='badge badge-secondary float-right mr-2 mt-4 text-center'><i className='fas fa-times-circle mr-1'></i>{filter.amenity}</span>
+            )}
           <Popover className='w-100' placement="bottom" isOpen={props.popoverOpen} target="Popover1" toggle={props.toggle}>
             <PopoverHeader className='text-center'>Filter Results</PopoverHeader>
             <PopoverBody>
-              <div className='form-group'>
-                <label>Price</label>
-                <select className='form-control'>
-                  <option>None</option>
-                  <option>less than $45</option>
-                  <option>less than $35</option>
-                  <option>less than $25</option>
-                </select>
-                <label className='mt-2'>Activities</label>
-                <select className='form-control'>
-                  <option>None</option>
-                  <option>Birding</option>
-                  <option>Boating</option>
-                  <option>Campfire</option>
-                  <option>Fishing</option>
-                  <option>Hiking</option>
-                  <option>Surfing</option>
-                  <option>Kayaking</option>
-                  <option>Swimming</option>
-                  <option>Boldering</option>
-                  <option>Rock climing</option>
-                </select>
-                <div className='form-check mt-3 ml-1 mb-3'>
-                  <input type='checkbox' className='form-check-input' />
-                  <div className='form-check-label h6'>Allows pets</div>
+              <form onSubmit={props.filterData}>
+                <div className='form-group'>
+                  <label>Price</label>
+                  <select name='price' className='form-control'>
+                    <option>None</option>
+                    <option value='45'>less than $45</option>
+                    <option value='35'>less than $35</option>
+                    <option value='25'>less than $25</option>
+                  </select>
+                  <label className='mt-2'>Activities</label>
+                  <select name='activity' className='form-control'>
+                    <option>None</option>
+                    <option>Birding</option>
+                    <option>Boating</option>
+                    <option>Campfire</option>
+                    <option>Fishing</option>
+                    <option>Hiking</option>
+                    <option>Surfing</option>
+                    <option>Kayaking</option>
+                    <option>Swimming</option>
+                    <option>Boldering</option>
+                    <option>Rock climing</option>
+                  </select>
+                  <div className='form-check mt-3 ml-1 mb-3'>
+                    <input name='pets' type='checkbox' className='form-check-input' />
+                    <div className='form-check-label h6'>Allows pets</div>
+                  </div>
+                  <button type='submit' className='btn btn-secondary filter-button mt-2'>Refine Search</button>
                 </div>
-                <button type='submit' className='btn btn-secondary filter-button mt-2'>Refine Search</button>
-              </div>
+              </form>
             </PopoverBody>
           </Popover>
         </div>
