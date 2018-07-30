4091dc2424ce2512abd3a6995741d1ba4e8cc1dc
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-07-30 14:56:50 -0700

    Converted navigation bar to a react strap component and added a cart button. #6

--- client/nav.js
+++ client/nav.js
@@ -1,20 +1,51 @@
 import React from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Col,
+  Row} from 'reactstrap';
 
-export default function Campgrounds(props) {
 
-  return (
-    <nav className='navbar navbar-expand-lg shadow bg-success main-nav p-0'>
-      <a className='navbar-brand ml-xl-4 ml-2 text-light' id='nav-brand' href='#campground-list'>
-        <span className='nav-logos'>
-          <i className='fas fa-fire logo-icon'></i>
-          <span className='logo-name pl-2'>Campr</span>
-        </span>
-      </a>
-      <ul className='navbar-nav ml-auto mr-4'>
-        <li className='nav-item active'>
-          <a className='nav-link home-link text-white' href='#campground-list'>Home</a>
-        </li>
-      </ul>
-    </nav>
-  )
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar className='main-nav' color='success' expand='md'>
+          <NavbarBrand className='text-white nav-logos' href='#campground-list'><i className='fas fa-fire logo-icon mr-2'></i>Campr</NavbarBrand>
+            <Nav className='ml-auto' navbar>
+              <Row>
+                <Col className='px-0'>
+                  <NavItem>
+                    <NavLink className='text-white home-link' href='#campground-list'>Home</NavLink>
+                  </NavItem>
+                </Col>
+                <Col className='pl-0 pr-3'>
+                  <NavItem>
+                    <NavLink className='text-white cart-link' href='#cart-view'><i className='fas fa-shopping-cart cart-icon mt-1 ml-2'></i></NavLink>
+                  </NavItem>
+                </Col>
+              </Row>
+            </Nav>
+        </Navbar>
+      </div>
+    );
+  }
 }
