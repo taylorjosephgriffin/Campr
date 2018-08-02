4e8ce8a547d6f42eef184df3e23f80642bf36e9a
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-02 15:09:48 -0700

    Separated the campground detail component from the carousel component. #6

--- client/components/campground-detail-header.js
+++ client/components/campground-detail-header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Service from '../util/service.js'
+import CampgroundDetailCarousel from './campground-detail-carousel.js'
 
 export default function CampgroundDetailHeader(props) {
 
@@ -59,28 +60,7 @@ export default function CampgroundDetailHeader(props) {
                     </div>
                   </div>
                 </div>
-                <div className='carousel col-xl-8 col-lg-12 col-md-12'>
-                  <div className='carousel-inner'>
-                    { props.campground.facilityPhotos.map((photo, index) =>
-                      <div key={index} className={ index === props.carouselIndex ? 'carousel-item active' : 'carousel-item'}>
-                        <img className='d-block w-100 h-100 detail-images' src={photo} alt={`slide #${index + 1}`}/>
-                        <ol className='carousel-indicators'>
-                          { props.campground.facilityPhotos.map((photo, index) =>
-                            <li key={index} className={ index === props.carouselIndex ? 'active' : 'not-active h-50'}></li>
-                          )}
-                        </ol>
-                        <a className='carousel-control-prev' onClick={props.carouselClick} role='button' data-slide='prev'>
-                          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-                          <span className='sr-only'>Previous</span>
-                        </a>
-                        <a className='carousel-control-next' onClick={props.carouselClick} role='button' data-slide='next'>
-                          <span className='carousel-control-next-icon' aria-hidden='true'></span>
-                          <span className='sr-only'>Next</span>
-                        </a>
-                      </div>
-                    )}
-                  </div>
-                </div>
+                <CampgroundDetailCarousel campground={props.campground} carouselClick={props.carouselClick} carouselIndex={props.carouselIndex}/>
               </div>
             </div>
           </div>
