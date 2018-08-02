7aa505e9f9998047dfe39ac30348364bd5e09b17
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-01 18:08:07 -0700

    Created a popover in the nav that shows a list of reservations in the cart. #6

--- client/nav.js
+++ client/nav.js
@@ -6,7 +6,11 @@ import {
   NavItem,
   NavLink,
   Col,
-  Row} from 'reactstrap';
+  Row,
+  Popover,
+  PopoverHeader,
+  PopoverBody,
+  Button } from 'reactstrap';
 
 
 export default class Navigation extends React.Component {
@@ -25,7 +29,18 @@ export default class Navigation extends React.Component {
     })
   }
 
+  componentDidMount() {
+    fetch('/reservations')
+      .then(res => res.json())
+      .then(reservation => {
+        this.setState({
+          reservations: reservation
+        })
+      })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <Navbar className='main-nav fixed-top shadow' color='success' expand='md'>
@@ -39,7 +54,26 @@ export default class Navigation extends React.Component {
                 </Col>
                 <Col className='pl-0 pr-3'>
                   <NavItem>
-                    <NavLink className='text-white cart-link' href='#cart-view'><i className='fas fa-shopping-cart cart-icon mt-1 ml-2'></i></NavLink>
+                    <Button id='Popover1' onClick={this.toggle} className='text-white cart-link bg-transparent border-0'><i className='fas fa-shopping-cart cart-icon mt-1 ml-2'></i></Button>
+                      <Popover className='w-100 p-0' placement="bottom" isOpen={this.state.isOpen} target="Popover1" toggle={this.toggle}>
+                        <PopoverHeader className='text-center'>Reservations</PopoverHeader>
+                        <PopoverBody className='p-0'>
+                          { !this.state.reservations ? null : this.state.reservations.map((reservation, index) =>
+                            <a href={`#checkout?reservationId=${reservation.reservationId}`} onClick={this.toggle} key={index}>
+                              <PopoverBody className='reservation-popover rounded'>
+                                <Row>
+                                  <Col xl='12' lg='12'>
+                                    <div>{reservation.campsite.loop}</div>
+                                    <div>{`${reservation.reservation.startDate} - ${reservation.reservation.endDate}`}</div>
+                                    <div>{`Guests: ${reservation.reservation.guests}`}</div>
+                                  </Col>
+                                </Row>
+                              </PopoverBody>
+                            </a>
+                          )}
+
+                        </PopoverBody>
+                      </Popover>
                   </NavItem>
                 </Col>
               </Row>
