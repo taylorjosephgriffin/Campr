3f6b2b653a7b9e2c617d209378a37c0932803d43
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-03 10:08:48 -0700

    Refactored naming of param in map functon of campground-list.js. #6

--- client/components/campground-list.js
+++ client/components/campground-list.js
@@ -5,19 +5,19 @@ export default function CampgroundList(props) {
   return (
     <div className='container-fluid'>
       <div className='row'>
-        { props.campgrounds.map((camps, index) =>
+        { props.campgrounds.map((campground, index) =>
           <div key={index} className='col-xl-4 col-lg-6 mb-3 translate-2'>
-            <a className='campground-link' href={`#details?id=${camps.id}`}>
-              <div data-id={camps.id} className='card h-100 campground-card shadow'>
+            <a className='campground-link' href={`#details?id=${campground.id}`}>
+              <div data-id={campground.id} className='card h-100 campground-card shadow'>
                 <div
                   className='card-img-top shadow campground-list-image h-100'
-                  style={{backgroundImage: `url(${camps.facilityPhotos[0]})`,
+                  style={{backgroundImage: `url(${campground.facilityPhotos[0]})`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover'}}>
-                  <h5 className='card-title py-2 campground-name text-light text-center'>{camps.facilityName}</h5>
+                  <h5 className='card-title py-2 campground-name text-light text-center'>{campground.facilityName}</h5>
                 </div>
                 <div className='card-body campground-body py-2 pl-2'>
-                  <p className='card-text h5 mt-4'>{`$${camps.price}.00 / night`}</p>
+                  <p className='card-text h5 mt-4'>{`$${campground.price}.00 / night`}</p>
                 </div>
               </div>
             </a>
