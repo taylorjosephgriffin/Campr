689f1dc367719206d92c0bf05b6952b22028e4b2
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-03 10:35:04 -0700

    Removed unused renderPrice and renderDetail methods. #6

--- client/containers/checkout-wizard.js
+++ client/containers/checkout-wizard.js
@@ -33,8 +33,6 @@ export default class CheckoutWizard extends React.Component {
         confirm: 33.33
       }
     }
-    this.renderDetails = this.renderDetails.bind(this)
-    this.renderPrice = this.renderPrice.bind(this)
     this.refreshReservations = this.refreshReservations.bind(this)
   }
 
@@ -56,24 +54,6 @@ export default class CheckoutWizard extends React.Component {
     if (this.props.params !== prevProps.params) {
       this.refreshReservations()
     }
-  }
-
-  renderDetails(label, data, style, xlSize, smSize, xsSize) {
-    return <Col style={style} xl={xlSize} lg={xlSize} md={xlSize} sm={smSize} xs={xsSize}>
-      <Label>{label}</Label>
-      <h5>{data}</h5>
-    </Col>
-  }
-
-  renderPrice(label, data, style, size, className) {
-    return <Fragment>
-      <Col style={style} xl={size} lg={size} sm={size} xs={size}>
-        <h4 className={className}>{label}</h4>
-      </Col>
-      <Col style={style} xl={size} lg={size} sm={size} xs={size}>
-        <h4 className={`float-right ${className}`}>{data}</h4>
-      </Col>
-    </Fragment>
   }
 
   render() {
