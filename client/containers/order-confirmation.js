c43086d1bb5bf62447b01554761bb9440e24b811
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-10 16:31:20 -0700

    Enhanced order canfirmation page to update when the orderId query parameter changes. #9

--- client/containers/order-confirmation.js
+++ client/containers/order-confirmation.js
@@ -8,9 +8,14 @@ export default class OrderConfirmation extends React.Component {
     this.state = {
       currentOrder: null
     }
+    this.refreshCurrentOrder = this.refreshCurrentOrder.bind(this)
   }
 
   componentDidMount() {
+    this.refreshCurrentOrder()
+  }
+
+  refreshCurrentOrder() {
     fetch('/orders/' + this.props.params.orderId)
       .then(res => res.json())
       .then(order => {
@@ -18,6 +23,12 @@ export default class OrderConfirmation extends React.Component {
           currentOrder: order
         })
       })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.params !== this.props.params) {
+      this.refreshCurrentOrder()
+    }
   }
 
   render() {
