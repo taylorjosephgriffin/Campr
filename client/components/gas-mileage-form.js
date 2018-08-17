60854c12961c180b2db7123ee645f046d336ba55
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-16 17:18:17 -0700

    Enhanced GasMileageForm by separating the inputs into a separate component for readability. #11

--- client/components/gas-mileage-form.js
+++ client/components/gas-mileage-form.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import {
-  Row,
-  Col,
-  Form,
-  FormGroup,
-  Input,
-  Button } from 'reactstrap'
+import { Row, Form, Button } from 'reactstrap'
 import GasMileageInfo from './gas-mileage-info.js'
 import GasMileageRadios from './gas-mileage-radios.js'
+import GasMileageInputs from './gas-mileage-inputs.js'
 
 export default function GasMileageForm(props) {
   return (
@@ -19,30 +14,26 @@ export default function GasMileageForm(props) {
         <GasMileageRadios value='van' icon='assets/van-icon.png' />
       </Row>
       <Row>
-        <Col xl='6'>
-          <legend>Vehicle <h6 className='d-inline'>year, make, model</h6></legend>
-          <FormGroup>
-            <Input type='text' name='vehicleInfo' required/>
-          </FormGroup>
-        </Col>
-        <Col xl='6'>
-          <legend>Distance <h6 className='d-inline'>in Miles</h6></legend>
-          <FormGroup>
-            <Input type='number' name='distance' required/>
-          </FormGroup>
-        </Col>
-        <Col xl='6'>
-          <legend>Fuel Efficiency <h6 className='d-inline'>in MPG</h6></legend>
-          <FormGroup>
-            <Input type='number' name='mileage' required/>
-          </FormGroup>
-        </Col>
-        <Col xl='6'>
-          <legend>Fuel Price <h6 className='d-inline'>per Gallon</h6></legend>
-          <FormGroup>
-            <Input type='input' name='price' required/>
-          </FormGroup>
-        </Col>
+        <GasMileageInputs
+          label='Vehicle'
+          byline='year, make, model'
+          type='text'
+          name='vehicleInfo' />
+        <GasMileageInputs
+          label='Distance'
+          byline='in Miles'
+          type='number'
+          name='distance' />
+        <GasMileageInputs
+          label='Fuel Efficiency'
+          byline='in MPG'
+          type='number'
+          name='mileage' />
+        <GasMileageInputs
+          label='Fuel Price'
+          byline='per Gallon'
+          type='input'
+          name='price' />
       </Row>
       <GasMileageInfo tripInfo={props.tripInfo} />
       <Row className='justify-content-center'>
