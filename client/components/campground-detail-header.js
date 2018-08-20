2c6ff80c31feb060154224e8d65f72b751b92700
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-19 18:00:15 -0700

    Converted component to full reactstrap. #13

--- client/components/campground-detail-header.js
+++ client/components/campground-detail-header.js
@@ -1,76 +1,112 @@
 import React from 'react'
 import Service from './service.js'
 import CampgroundDetailCarousel from './campground-detail-carousel.js'
+import { Container,
+  Row,
+  Col,
+  CardBody,
+  Card,
+  CardTitle,
+  ListGroupItem,
+  ListGroup,
+  Button
+} from 'reactstrap'
+
+const buttonStyle = {
+  height: '50px'
+}
+const heartStyle = {
+  fontSize: '35px'
+}
+const campgroundCardStyle = {
+  minHeight: '400px'
+}
+const campgroundNameStyle = {
+  overflowWrap: 'normal'
+}
+const hrDetailStyle = {
+  borderTop: '3px double #8c8b8b'
+}
 
 export default function CampgroundDetailHeader(props) {
 
   return (
-    <div className='container-fluid mt-5 pt-5'>
-      <div className='row'>
-        <div className='col-xl-12'>
-          <div className='campground-detail-card'>
-            <div className='card-body'>
-              <div className='row card-detail-page'>
-                <div className='card col-xl-4 col-lg-12 col-md-12 col-12 camp-detail-name'>
-                  <hr className='hr-detail'/>
-                  <h1 className='card-title py-2 text-center text-dark mt-3'>{ props.campground.facilityName }</h1>
-                  <hr className='hr-detail'/>
-                  <div className='container-fluid'>
-                    <div className='row mt-4'>
-                      <h3 className='text-dark'><u>Activities</u></h3>
-                    </div>
-                    <div className='row'>
+    <Container fluid className='mt-5 pt-5'>
+      <Row>
+        <Col xl='12'>
+          <CardBody style={campgroundCardStyle}>
+            <Row className='card-detail-page'>
+              <Col xl='4' lg='12' md='12' sm='12' xs='12'>
+                <Card style={campgroundNameStyle}>
+                  <hr style={hrDetailStyle}/>
+                  <CardTitle className='h1 py-2 text-center text-dark mt-3'>{ props.campground.facilityName }</CardTitle>
+                  <hr style={hrDetailStyle}/>
+                  <Container fluid>
+                    <Row className='mt-4'>
+                      <Col>
+                        <h3 className='text-dark'><u>Activities</u></h3>
+                      </Col>
+                    </Row>
+                    <Row>
                       { props.campground.amenities.map((amenity, index) =>
-                        <ul key={index} className='list-group col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12 text-center'>
-                          <li className='list-group-item mt-3'>{amenity}</li>
-                        </ul>
+                        <Col key={index} xl='4' lg='6' md='6' sm='6' xs='12'>
+                          <ul className='list-group text-center'>
+                            <li className='list-group-item mt-3'>{amenity}</li>
+                          </ul>
+                        </Col>
                       )}
-                    </div>
-                    <div className='row mt-3'>
-                      <div className='col pl-0'>
+                    </Row>
+                    <Row className='mt-3'>
+                      <Col>
                         <h3 className='text-dark'><u>Accomodations</u></h3>
-                      </div>
-                    </div>
-                    <div className='row mt-1'>
+                      </Col>
+                    </Row>
+                    <Row className='mt-1'>
                       <Service hasService={props.campground.petsAllowed} service={'Allows pets'} />
                       <Service hasService={props.campground.hasFireRing} service={'Fire Rings'} />
                       <Service hasService={props.campground.hasAmps} service={'Amps'} />
                       <Service hasService={props.campground.hasSewer} service={'Sewer'} />
                       <Service hasService={props.campground.hasWater} service={'Water'} />
                       <Service hasService={props.campground.hasRestroom} service={'Restroom'} />
-                    </div>
-                    <div className='row mt-4'>
-                      <ul className='list-group col-xl-6 col-lg-6 col-md-6 col-sm-6 mt-2 col-12'>
-                        <li className='list-group-item'>Check-in: {props.campground.checkIn}:00pm</li>
-                      </ul>
-                      <ul className='list-group col-xl-6 col-lg-6 col-md-6 col-sm-6 mt-2 col-12'>
-                        <li className='list-group-item'>
-                          Check-out: {props.campground.checkOut}:00pm
-                        </li>
-                      </ul>
-                    </div>
-                    <div className='row justify-content-end'>
-                      <div className='col-xl-4 col-lg-4 col-6 mt-5 pb-4 pl-0 translate-2'>
-                        <button className='btn btn-secondary shadow view-campsites mt-4 w-100'>
-                          Add to Favorites
-                        </button>
-                      </div>
-                      <div className='col-xl-4 col-lg-4 col-6 mt-5 pb-4 pl-0 translate-2'>
+                    </Row>
+                    <Row className='mt-4'>
+                      <Col xl='6' lg='6' md='6' sm='6' xs='12'>
+                        <ListGroup className='mt-2'>
+                          <ListGroupItem>
+                            Check-in: {props.campground.checkIn}:00pm
+                          </ListGroupItem>
+                        </ListGroup>
+                      </Col>
+                      <Col xl='6' lg='6' md='6' sm='6' xs='12'>
+                        <ListGroup className='mt-2'>
+                          <ListGroupItem>
+                            Check-out: {props.campground.checkOut}:00pm
+                          </ListGroupItem>
+                        </ListGroup>
+                      </Col>
+                    </Row>
+                    <Row className='justify-content-end'>
+                      <Col xl='7' lg='8' md='6' sm='6' xs='6' className='mt-5 pb-4'>
+                        <Button color='transparent' style={buttonStyle} className='mt-4 float-left'>
+                          <i style={heartStyle} className='far fa-heart text-secondary'></i>
+                        </Button>
+                      </Col>
+                      <Col xl='5' lg='4' md='6' sm='6' xs='6' className='mt-5 pb-4'>
                         <a href={`#campsite?facilityNum=${props.campground.facilityNum}&id=${props.campground.id}`}>
-                          <button className='btn btn-primary shadow view-campsites mt-4 w-100'>
+                          <Button color='primary' style={buttonStyle} className='shadow mt-4 w-100 float-right'>
                             Campsites
-                          </button>
+                          </Button>
                         </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <CampgroundDetailCarousel campground={props.campground} carouselClick={props.carouselClick} carouselIndex={props.carouselIndex}/>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                      </Col>
+                    </Row>
+                  </Container>
+                </Card>
+              </Col>
+              <CampgroundDetailCarousel campground={props.campground} carouselClick={props.carouselClick} carouselIndex={props.carouselIndex}/>
+            </Row>
+          </CardBody>
+        </Col>
+      </Row>
+    </Container>
   )
 }
