281fcaa6105915a827b5a00287703237e65adee4
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-20 22:03:53 -0700

    Enhanced the campgroundDetail component to update the campground currently in view when a favorite is clicked. #13

--- client/containers/campground-detail.js
+++ client/containers/campground-detail.js
@@ -9,9 +9,10 @@ export default class CampgroundDetail extends React.Component {
       carouselIndex: 0
     }
     this.carouselClick = this.carouselClick.bind(this)
+    this.refreshCurrent = this.refreshCurrent.bind(this)
   }
 
-  componentDidMount() {
+  refreshCurrent() {
     fetch('/campgrounds/' + this.props.params.id)
       .then(res => res.json())
       .then(camp => {
@@ -27,6 +28,17 @@ export default class CampgroundDetail extends React.Component {
           campsites: sites
         })
       })
+  }
+
+  componentDidMount() {
+    this.refreshCurrent()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.params !== this.props.params) {
+      this.refreshCurrent()
+      window.scrollTo(0, 0)
+    }
   }
 
   carouselClick(event) {
