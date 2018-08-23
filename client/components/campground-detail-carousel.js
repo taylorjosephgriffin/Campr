795be9f5aa14fc5529955474ebc4d88714693916
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-22 19:26:52 -0700

    Enhanced carousel component. #35

--- client/components/campground-detail-carousel.js
+++ client/components/campground-detail-carousel.js
@@ -10,14 +10,14 @@ export default function CampgroundDetailCarousel(props) {
             <img className='d-block w-100 h-100 detail-images' src={photo} alt={`slide #${index + 1}`}/>
             <ol className='carousel-indicators'>
               { props.campground.facilityPhotos.map((photo, index) =>
-                <li key={index} className={ index === props.carouselIndex ? 'active' : 'not-active h-50'}></li>
+                <li key={index} style={ index === props.carouselIndex ? {backgroundColor: 'white'} : {backgroundColor: 'rgba(0, 0, 0, 0.5)'} } className={ index === props.carouselIndex ? 'active' : 'not-active' }></li>
               )}
             </ol>
-            <a className='carousel-control-prev' onClick={props.carouselClick} role='button' data-slide='prev'>
+            <a className='carousel-control-prev' style={{background: 'linear-gradient(90deg, rgba(0, 0, 0, 0.8), transparent)'}} onClick={props.carouselClick} role='button' data-slide='prev'>
               <span className='carousel-control-prev-icon' aria-hidden='true'></span>
               <span className='sr-only'>Previous</span>
             </a>
-            <a className='carousel-control-next' onClick={props.carouselClick} role='button' data-slide='next'>
+            <a className='carousel-control-next' style={{background: 'linear-gradient(-90deg, rgba(0, 0, 0, 0.8), transparent)'}} onClick={props.carouselClick} role='button' data-slide='next'>
               <span className='carousel-control-next-icon' aria-hidden='true'></span>
               <span className='sr-only'>Next</span>
             </a>
