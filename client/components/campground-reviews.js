670dd358ac0ec2cdd2fa2fa8347bb5de01f3a051
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-08-31 13:58:14 -0700

    Enhanced the CampgroundReviews component to render the newly created modal when 'Leave a Review' is clicked. #32

--- client/components/campground-reviews.js
+++ client/components/campground-reviews.js
@@ -5,29 +5,46 @@ import {
   Card,
   CardHeader,
   CardText,
+  Button,
   CardBody } from 'reactstrap'
+import ReviewModal from './review-modal.js'
 
 export default class CampgroundReviews extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      isOpen: false
+    }
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
   }
 
   render() {
     return (
-      <CardBody>
-        <Row>
-          <Col xl='12'>
-            <Card className='mt-3'>
-              <CardHeader className='h1'>Reviews</CardHeader>
-              <CardBody style={{height: '400px', overflow: 'scroll'}}>
-                <CardText>No reviews.</CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </CardBody>
+      <div>
+        <CardBody>
+          <Row>
+            <Col xl='12'>
+              <Card className='mt-3'>
+                <CardHeader className='h1'>Reviews<Button onClick={this.toggle} className='float-right'>Leave a Review</Button></CardHeader>
+                <CardBody style={{height: '400px', overflow: 'scroll'}}>
+                  <CardText>No reviews.</CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </CardBody>
+        <ReviewModal
+          isOpen={this.state.isOpen}
+          toggleReviewModal={this.toggle}
+          params={this.props.params}/>
+      </div>
     )
   }
 }
