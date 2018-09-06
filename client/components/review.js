d218a0154d7303e7f27170a00073b4a3be72e97e
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-09-05 17:44:38 -0700

    Created a new component that will be used to render the reviews. #43

---
+++ client/components/review.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  Col,
+  Card,
+  Container,
+  CardBody,
+  Row } from 'reactstrap'
+import Rating from 'react-rating'
+
+export default function RenderReview(props) {
+
+  return (
+    <Card>
+      <Container>
+        <CardBody>
+          <Row>
+            <Col xl='2' lg='2' md='3' sm='12' xs='12'>
+              <img style={{width: '100%'}} className='py-2' src='./assets/review-icon.png' />
+              <div className='text-center text-dark'>{props.review.name}</div>
+              <div className='text-center text-dark'>{props.review.location}</div>
+              <div className='text-center text-success'>
+                <Rating
+                  readonly={true}
+                  initialRating={Number(props.review.rating)}
+                  emptySymbol='far fa-star'
+                  fullSymbol='fas fa-star' />
+              </div>
+            </Col>
+            <Col xl='10' lg='10' md='9' sm='12' xs='12'>
+              <div className='py-2 text-dark'>{props.review.review}</div>
+            </Col>
+          </Row>
+        </CardBody>
+      </Container>
+    </Card>
+  )
+}
